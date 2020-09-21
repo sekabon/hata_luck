3fbef813c654255a719868284ba4ebe6af385304
sekabon <66765282+sekabon@users.noreply.github.com>
2020-09-21 17:23:46 +0900

    予定保存機能実装

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,6 +1,22 @@
 class ShiftsController < ApplicationController
   def index
     @user = User.all
+    @shift = Shift.all
   end
-
+  def new
+    @shift = Shift.new
+  end
+  def create
+    @shift = Shift.new(shift_params)
+    if @shift.valid?
+      @shift.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  private
+  def shift_params
+    params.permit(:work_day, :start_time, :end_time, :break_start, :break_end).merge(user_id: current_user.id)
+  end
 end
