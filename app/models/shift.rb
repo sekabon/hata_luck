02e958755da644988fe1bc731a8661c051be0581
sekabon <66765282+sekabon@users.noreply.github.com>
2020-10-01 10:24:32 +0900

    時間のバリデーション実装

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -8,7 +8,16 @@ class Shift < ApplicationRecord
   belongs_to :user
   validates :work_day, timeliness: { on_or_after: Time.now.since(2.weeks).monday }
   
-  validates :start_time, numericality: { other_than: 0, message: "can't be blank" }
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validate :end_time_is_after_start_time
+
+  def end_time_is_after_start_time
+    return if end_time.blank? || start_time.blank?
   
+    if end_time < start_time
+      errors.add(:end_time, "は出勤時間より前にしてください") 
+    end 
+  end
 
 end
