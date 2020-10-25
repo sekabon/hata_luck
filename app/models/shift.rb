6967023465ba3c7ca64d6f2e301c6e0b2fba9a66
sekabon <66765282+sekabon@users.noreply.github.com>
2020-10-25 17:51:32 +0900

    validate set

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -19,18 +19,18 @@ class Shift < ApplicationRecord
 
   def end_time_is_after_start_time
     return if end_time.blank? || start_time.blank?
-    errors.add(:end_time, 'は出勤時間より前にしてください') if  end_time < start_time
+    errors.add(:end_time, 'は出勤時間より後にしてください') if  end_time < start_time
   end
   def break_end_is_after_break_start
     return if break_end.blank? || break_start.blank?
-    errors.add(:break_end, 'は休憩開始時間より前にしてください') if break_end < break_start
+    errors.add(:break_end, 'は休憩開始時間より後にしてください') if break_end < break_start
   end
   def end_time_is_after_break_end
     return if end_time.blank? || break_end.blank?
-    errors.add(:end_time, 'は休憩終了時間より前にしてください') if end_time < break_end
+    errors.add(:end_time, 'は休憩終了時間より後にしてください') if end_time < break_end
   end
   def break_start_is_after_start_time
     return if break_start.blank? || start_time.blank?
-    errors.add(:break_start, 'は出勤時間より前にしてください') if break_start < start_time
+    errors.add(:break_start, 'は出勤時間より後にしてください') if break_start < start_time
   end
 end
